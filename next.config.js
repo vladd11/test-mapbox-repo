1a4b31052b54194bfbe11bd569b97d9fc55082af
vladd11 <rozhkov.2006@gmail.com>
2022-04-19 16:51:24 +0400

    Initial

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        domains: ['2abf8861.site-deploy.pages.dev'],
+        formats: ['image/avif', "image/webp"],
+        loader: 'custom'
+    },
+    assetPrefix: './'
 }
 
 module.exports = nextConfig
